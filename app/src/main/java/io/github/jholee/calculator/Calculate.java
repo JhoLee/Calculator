a4fd72be7ab2a196beed3345e20e47fa91d20849
JhoLee <jho3689@a.ut.ac.kr>
2018-02-16 15:18:55 +0900

    Initial commit

---
+++ app/src/main/java/io/github/jholee/calculator/Calculate.java
@@ -0,0 +1,19 @@
+package io.github.jholee.calculator;
+
+
+/**
+ * Created by Jho on 2018-02-10.
+ */
+
+public class Calculate {
+
+
+    static final int PLUS = 0;
+    static final int MINUS = 1;
+    static final int MULTIPLY = 2;
+    static final int DIVIDE = 3;
+
+
+    // TODO: Map 사용해서, 연산식 해석.
+
+}
